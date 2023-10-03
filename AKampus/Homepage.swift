250fd3952154bae46f191092c32ed65a78081044
İsmail Nermiş <39004389+ismailnermis@users.noreply.github.com>
2023-10-03 23:07:39 +0300

    Camera settings done

--- AKampus/Homepage.swift
+++ AKampus/Homepage.swift
@@ -14,14 +14,19 @@ class Homepage: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        labelMessage.alpha = 0
         
     }
-    
+    // MARK: - Camera Button
     @IBAction func buttonCamera(_ sender: UIButton) {
-        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        present(imagePicker, animated: true)
     }
     
+    // MARK: - Gallery Button
     @IBAction func buttonGallery(_ sender: Any) {
         
     }
@@ -29,3 +34,14 @@ class Homepage: UIViewController {
     
 }
 
+// MARK: - Extension : UIImagePickerControllerDelegate, UINavigationControllerDelegate
+extension Homepage : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // MARK: - ImagePicker Func
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            print("Success")
+        }
+        picker.dismiss(animated: true)
+    }
+}
