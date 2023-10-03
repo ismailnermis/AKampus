fe3dcc0602aa1270a0f74377d3c76e22f0ea34fd
İsmail Nermiş <39004389+ismailnermis@users.noreply.github.com>
2023-10-03 22:53:02 +0300

    Design complete

---
+++ AKampus/Homepage.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  AKampus
+//
+//  Created by İsmail Nermiş on 3.10.2023.
+//
+
+import UIKit
+
+class Homepage: UIViewController {
+    
+    @IBOutlet weak var labelMessage: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+    @IBAction func buttonCamera(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func buttonGallery(_ sender: Any) {
+        
+    }
+    
+    
+}
+
